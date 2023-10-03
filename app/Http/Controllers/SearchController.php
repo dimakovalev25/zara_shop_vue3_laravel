039e380ac4bc8602318ef5e58e9375926d75fe35
dima25 <d.nozdryn@gmail.com>
2023-10-03 18:07:39 +0300

    start change language

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Filters\ProductFilter;
+use App\Http\Requests\FilterRequest;
+use App\Models\Product;
+
+
+class SearchController extends Controller
+{
+    public function index(FilterRequest $request)
+    {
+ /*       $data = $request->validated();
+        $filter = app()->make(ProductFilter::class, ['queryParams' => array_filter($data)]);
+        $products = Product::query()->filter($filter)->paginate(4)->withQueryString();*/
+
+
+
+
+        $title = $request['title'];
+        $products = Product::where('title', 'like', '%' . $title . '%')->paginate(12);
+        return view('product.index', compact('products'));
+
+    }
+}
